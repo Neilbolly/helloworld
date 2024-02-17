9aa99d090bdcd9faf13a1e1a1f9507d1af994f1b
Neilbolly <147328874+Neilbolly@users.noreply.github.com>
2024-02-18 01:19:31 +0530

    Update hello_world.rs

--- hello_world.rs
+++ hello_world.rs
@@ -1,3 +1,9 @@
-fn main() {
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+    let mut stdout = io::stdout();
+    stdout.write_all(b"Error\n")?;
+
+    Ok(())
 }
